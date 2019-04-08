ea98099f0f7b9c9b09f3c61329d31329d624aeec
github username <github email address>
2019-04-08 12:58:50 -0400

    first commit

---
+++ components/Page.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Meta from '../components/Meta';
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+    background: white;
+    color: black;
+`;
+
+const Inner = styled.div `
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 2rem;
+`;
+
+class Page extends Component {
+    render() {
+        return (
+            <StyledPage>
+                <Meta />
+                <Header />
+                <Inner>{this.props.children}</Inner>
+            </StyledPage>
+        );
+    }
+}
+
+export default Page;
---
+++ pages/love.js
@@ -0,0 +1,7 @@
+const Love = props => (
+    <div>
+        <p>Hey love!</p>
+    </div>
+)
+
+export default Love
